c8844f4fec93a778d28a93bc08fbce4e2a40d3b9
Daniel Renz <daniel.l.renz@gmail.com>
2018-09-25 15:17:15 +0200

    added styled components

--- src/SplashPage.js
+++ src/SplashPage.js
@@ -1,12 +1,29 @@
 import { h, Component } from 'preact';
+import styled from 'styled-components';
+
+import theme from './styles/theme';
+
+const StyledPage = styled.div`
+	margin: 10px 10px 10px 10px;
+`;
+
+const StyledLink = styled.a`
+	background: ${theme.button.bg};
+	color: ${theme.button.fg};
+	text-decoration: none;
+	padding: 10px 20px;
+	box-shadow: 2px 2px 2px #bbbbbb;
+	margin: 5px;
+	border: 1px solid ${theme.button.border};
+`;
 
 class SplashPage extends Component {
   render() {
     return (
-      <div className='SplashPage'>
-        <a href='/edit'>Edit</a><br/>
-        <a href='/view'>View</a>
-      </div>
+      <StyledPage className='SplashPage'>
+        <StyledLink href='/edit'>Edit</StyledLink>
+        <StyledLink href='/view'>View</StyledLink>
+      </StyledPage>
     );
   }
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+	button: {
+		bg: 'rgba(0,0,255,0.3)',
+		fg: 'rgba(0,50,0,1)',
+		border: 'rgba(0,0,255,0.1)',
+	}
+}
+
+export default theme;
